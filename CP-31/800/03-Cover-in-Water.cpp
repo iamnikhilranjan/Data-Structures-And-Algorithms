c1b1276d5c902398be30210f98104c89b7180f0c
iamnikhilranjan <nikhil21907@gmail.com>
2025-03-25 11:57:08 +0530

    solved 03 - 04

--- CP-31/800/03-Cover-in-Water.cpp
+++ CP-31/800/03-Cover-in-Water.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    while(t--) {
-        int n ;
+    while (t--)
+    {   
+        int n;
         string s;
-        cin >> s;
-        
+        cin >> n >> s;
+        int totalDot = 0;
+        int maxDots = 0;
+        int ans = 0;
+        for (auto ch : s)
+        {
+            if (ch == '.')
+            {
+                totalDot++;
+                maxDots++;
+                if (maxDots >= 3)
+                {
+                    ans = 2;
+                    break;
+                }
+            }
+            else
+            {
+                maxDots = 0;
+            }
+        }
+        if(ans != 0) cout << ans << endl;
+        else cout << totalDot << endl;
     }
 }
